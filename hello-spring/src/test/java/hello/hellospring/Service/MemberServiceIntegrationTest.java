772a9f8cba5db8ce1e5fb4847cd3ad2511069dbe
BlowF1y <blowfly0258@gmail.com>
2022-03-28 20:40:47 +0900

    3.28공부한거

---
+++ hello-spring/src/test/java/hello/hellospring/Service/MemberServiceIntegrationTest.java
@@ -0,0 +1,55 @@
+package hello.hellospring.Service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+
+
+    @Test
+    void 화원가입() {
+        //given
+        Member member = new Member();
+        member.setName("member");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member findmember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findmember.getName());
+    }
+
+    @Test
+    public void 중복회원검증(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        memberService.join(member1);
+
+
+    }
+
+}
