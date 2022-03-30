1b4839ab66d12db62633239e189b2b6bbf7f05eb
BlowF1y <blowfly0258@gmail.com>
2022-03-30 21:01:41 +0900

    3.30공부한거

--- hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
@@ -5,9 +5,11 @@ import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -17,6 +19,7 @@ public class MemberService {
     }
 
     public Long join(Member member){
+
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
--- hello-spring/src/test/java/hello/hellospring/Service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/Service/MemberServiceIntegrationTest.java
@@ -14,6 +14,7 @@ import javax.transaction.Transactional;
 
 
 @SpringBootTest
+
 @Transactional
 class MemberServiceIntegrationTest {
 
