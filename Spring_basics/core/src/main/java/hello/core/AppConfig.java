1a2d8d97cb070b0fea17e2fe294c5f8b403877b2
BlowF1y <blowfly0258@gmail.com>
2022-05-05 15:56:57 +0900

    05.05 commit

--- Spring_basics/core/src/main/java/hello/core/AppConfig.java
+++ Spring_basics/core/src/main/java/hello/core/AppConfig.java
@@ -5,15 +5,26 @@ import hello.core.Member.MemberServiceImple;
 import hello.core.Member.MemoryMemberRepository;
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 
 public class AppConfig {
     //생성자 주입
     public MemberService memberService() {
-        return new MemberServiceImple(new MemoryMemberRepository());
+        return new MemberServiceImple(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
     }
 }
